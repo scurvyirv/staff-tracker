f1229e4161b684cc0797fa703b6f2dc95a050f70
Irvin Lu <scurvyirv@scurvyirvy.attlocal.net>
2024-06-26 23:32:05 -0700

    this commit adds more logic to database.js, fixes syntax issues in options.js and requires additional testing of the add functions

--- database.js
+++ database.js
@@ -3,17 +3,42 @@
 //import database connection
 const pool = require('./db.js');
 
-//handles updating database when adding new employees
+//2nd handles updating database when adding new employees
 async function addEmployeeToDatabase(firstName, lastName, roleId) {
     try {
-        const queryText = 'INSERT INTO employees (first_name, last_name, role_id) VALUES ($1, $2, $3)';
+        const queryText = 'INSERT INTO employees (first_name, last_name, role_id) VALUES ($1, $2, $3);';
         const values = [firstName, lastName, roleId];
         await db.query(queryText, values);
     } catch (error) {
         console.error("Error adding employee to the database", error);
         throw error;
     }
-}
+};
+
+//5th handles updating database when adding new role
+async function addRoleToDatabase(title, departmentId, salary) {
+    try {
+        const queryText = 'INSERT INTO roles (title, department, salary) VALUES ($1, $2, $3);';
+        const values = [title, departmentId, salary];
+        await db.query(queryText, values);
+    } catch (error) {
+        console.error("error adding role to database", error);
+        throw error;
+    }
+};
+
+//7th handles updating database when adding new department
+async function addDepartmentToDatabase(department_name) {
+    try {
+        const queryText = 'INSERT INTO departments (department_name) VALUES ($1);';
+        const values = [department_name];
+        await db.query(queryText, values);
+    } catch (error) {
+        console.error("error adding department to database", error);
+        throw error;
+    }
+};
+
 
 module.exports = {
     addEmployeeToDatabase,
